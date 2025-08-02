2a7dbf809f2e84b47eff62f1bee00f7c2db8938e
Hiroaki Yutani <yutani.ini@gmail.com>
2025-08-02 10:43:40 +0900

    Revert

--- tools/config.R
+++ tools/config.R
@@ -14,17 +14,6 @@ vendor_exists <- file.exists("src/rust/vendor.tar.xz")
 is_not_cran <- env_not_cran != ""
 is_debug <- env_debug != ""
 
-# We specify this target when building for webR
-webr_target <- "wasm32-unknown-emscripten"
-
-# here we check if the platform we are building for is webr
-is_wasm <- identical(R.version$platform, webr_target)
-
-# print to terminal to inform we are building for webr
-if (is_wasm) {
-  message("Building for WebR")
-}
-
 if (is_debug) {
   # if we have DEBUG then we set not cran to true
   # CRAN is always release build
@@ -39,14 +28,25 @@ if (!is_not_cran) {
 # we set cran flags only if NOT_CRAN is empty and if
 # the vendored crates are present.
 .cran_flags <- ifelse(
-  !is_not_cran && vendor_exists && !is_wasm,
+  !is_not_cran && vendor_exists,
   "-j 2 --offline",
   ""
 )
 
 # when DEBUG env var is present we use `--debug` build
-.profile <- ifelse(is_debug, ifelse(is_wasm, "--profile webr", ""), "--release")
+.profile <- ifelse(is_debug, "", "--release")
 .clean_targets <- ifelse(is_debug, "", "$(TARGET_DIR)")
+
+# We specify this target when building for webR
+webr_target <- "wasm32-unknown-emscripten"
+
+# here we check if the platform we are building for is webr
+is_wasm <- identical(R.version$platform, webr_target)
+
+# print to terminal to inform we are building for webr
+if (is_wasm) {
+  message("Building for WebR")
+}
 
 # we check if we are making a debug build or not
 # if so, the LIBDIR environment variable becomes:
@@ -62,11 +62,6 @@ cfg <- if (is_debug) "debug" else "release"
 # we specify the target _only_ on webR
 # there may be use cases later where this can be adapted or expanded
 .target <- ifelse(is_wasm, paste0("--target=", webr_target), "")
-
-# webR-related
-.toolchain <- ifelse(is_wasm, "+nightly", "") # Use nightly toolchain for -Z flags
-.rustflags <- ifelse(is_wasm, "-Zdefault-visibility=hidden", "")
-.cargoflags <- ifelse(is_wasm, "-Zbuild-std=panic_abort,std", "")
 
 # read in the Makevars.in file checking
 is_windows <- .Platform[["OS.type"]] == "windows"
@@ -99,10 +94,7 @@ new_txt <- gsub("@CRAN_FLAGS@", .cran_flags, mv_txt) |>
   gsub("@PROFILE@", .profile, x = _) |>
   gsub("@CLEAN_TARGET@", .clean_targets, x = _) |>
   gsub("@LIBDIR@", .libdir, x = _) |>
-  gsub("@TARGET@", .target, x = _) |>
-  gsub("@TOOLCHAIN@", .toolchain, x = _) |>
-  gsub("@RUSTFLAGS@", .rustflags, x = _) |>
-  gsub("@CARGOFLAGS@", .cargoflags, x = _)
+  gsub("@TARGET@", .target, x = _)
 
 message("Writing `", mv_ofp, "`.")
 con <- file(mv_ofp, open = "wb")
